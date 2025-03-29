27b9527514ca41ce4742cbecbb598728350c042c
bkstory2 <bkstory@nate.com>
2025-03-29 13:01:41 +0900

    material

--- src/App.js
+++ src/App.js
@@ -1,52 +1,79 @@
 import './App.css';
 import { Component } from 'react';
 import Customer from './components/Customer';
+import { Table, TableHead, TableBody, TableRow, TableCell, TableContainer, Paper } from '@mui/material';
+import { withStyles } from '@mui/styles'; // ✅ 올바른 import
+
+const styles = (theme) => ({
+  root: {
+    width: '100%',   
+    overflowX: 'auto'
+  },
+  table: {
+    minWidth: 1080,
+  },
+});
 
 const customers = [
-      {
-      'id':1,
-      'image':'https://picsum.photos/201/300',
-      'name' :'황병규',
-      'birthday':'730222',
-      'gender':'남',
-      'job':'개발자222'
-      },
-      {
-        'id':2,
-        'image':'https://picsum.photos/202/300',
-        'name' :'이이이이',
-        'birthday':'880222',
-        'gender':'남',
-        'job':'개발자22'
-        }
-      ,{
-          'id':3,
-          'image':'https://picsum.photos/203/300',
-          'name' :'김미김김',
-          'birthday':'990222',
-          'gender':'남',
-          'job':'개발자3'
-       }
-]
+  {
+    id: 1,
+    image: 'https://picsum.photos/101/100',
+    name: '황병규',
+    birthday: '730222',
+    gender: '남',
+    job: '개발자222',
+  },
+  {
+    id: 2,
+    image: 'https://picsum.photos/102/100',
+    name: '이이이이',
+    birthday: '880222',
+    gender: '남',
+    job: '개발자22',
+  },
+  {
+    id: 3,
+    image: 'https://picsum.photos/103/100',
+    name: '김미김김',
+    birthday: '990222',
+    gender: '남',
+    job: '개발자3',
+  },
+];
 
-class App extends Component{
-    render() {
-        return(
-          <div>
-              {
-                 customers.map(c=>{
-                  return(
-                     <Customer 
-                        key={c.id} 
-                        id={c.id} image={c.image}  name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}
-                     />
-                  )
-                 })
-              }               
-            </div>
-        )
-    }
-
+class App extends Component {
+  render() {
+    const { classes } = this.props; // ✅ this.props에서 classes 추출
+    return (
+      <TableContainer component={Paper} className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map((c) => (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,41 +1,19 @@
-import React from "react";
+import { TableRow, TableCell } from '@mui/material';
+import React from 'react';
 
-class Customer extends React.Component {
-  
-    render(){
-        return(
-            <div>
-                <CustomerProfile id={this.props.id} name={this.props.name}  image={this.props.image}></CustomerProfile>
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender}  job={this.props.job}></CustomerInfo>
-            </div>
-        )
-    }
-}
+const Customer = ({ id, image, name, birthday, gender, job }) => {
+  return (
+    <TableRow>
+      <TableCell>{id}</TableCell>
+      <TableCell>
+        <img src={image} alt="profile" width="50" />
+      </TableCell>
+      <TableCell>{name}</TableCell>
+      <TableCell>{birthday}</TableCell>
+      <TableCell>{gender}</TableCell>
+      <TableCell>{job}</TableCell>
+    </TableRow>
+  );
+};
 
-class CustomerProfile extends React.Component{
-    render()
-    {
-        return(
-            <div>
-                <img src={this.props.image} alt="profile"></img>
-                <h2>{this.props.id}  {this.props.name}</h2>
-            </div>
-        )
-    }
-}
-
-class CustomerInfo  extends React.Component{
-    render()
-    {
-        return(
-            <div>
-                <h2>{this.props.birthday}  </h2>
-                <h2>{this.props.gender}  </h2>
-                <h2>{this.props.job} </h2>
-            </div>
-        )
-    }
-}
-
-
-export default Customer ; 
+export default Customer;
