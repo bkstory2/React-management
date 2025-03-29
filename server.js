b8264bd93710bfdf95050e7128b3c48b68012c38
bkstory2 <bkstory@nate.com>
2025-03-29 17:06:08 +0900

    aaaa

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const mysql = require('mysql');
 const cors = require('cors');
+const fs = require('fs');
+
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 // CORS 미들웨어를 app 객체가 생성된 후에 호출
 app.use(cors());
+
+
+// database.json 읽기
+const dbConfig = JSON.parse(fs.readFileSync('./database.json', 'utf8'));
+
+// MySQL 연결 설정
+const connection  = mysql.createConnection(dbConfig);
+
 
 // 요청이 들어오는지 확인하는 미들웨어
 app.use((req, res, next) => {
@@ -17,7 +28,8 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/customers', (req, res) => {
+
+app.get('/api/customers_sample', (req, res) => {
     console.log('Call client  ====> call');
   
     // 5초(5000ms) 지연 후 응답 전송
@@ -54,3 +66,28 @@ app.get('/api/customers', (req, res) => {
 app.listen(port, () => {
   console.log(`서버 동작 중 : ${port}`);
 });
+
+
+// MySQL 연결
+connection.connect((err) => {
+    if (err) {
+      console.error('MySQL 연결 실패:', err);
+      return;
+    }
+    console.log('MySQL 연결 성공');
+  });
+  
+  // 고객 목록 조회 API
+  app.get('/api/customers', (req, res) => {
+    console.log('고객 목록 요청 수신');
+    
+    const query = 'SELECT * FROM customer';
+  
+        connection.query(query, (err, results) => {
+            if (err) {
+                console.error('connection 조회 오류:', err);
+                return res.status(500).send('서버 오류');
+            }
+             res.json(results);
+        });
+  });
